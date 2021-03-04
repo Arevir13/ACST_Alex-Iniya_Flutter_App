2ded00f04adc36bbeab300ba5f02f77803de068e
Arevir13 <Alexriv131@gmail.com>
2021-03-04 09:02:50 -0600

    - Added new creationDate variable to agendas - changed the calendar format to be a table - Code to automatically add agendas to calendar (WIP) - Added datepicker to pick due dates for items (WIP) - added new dependency to pubspec.yaml

--- lib/Pages/calendarScreen.dart
+++ lib/Pages/calendarScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:practice_app/Custom_Widgets/Agenda_Widget.dart';
 import 'package:practice_app/Custom_Widgets/PublicVariables.dart' as globals;
-import 'package:calendar_timeline/calendar_timeline.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarScreen extends StatefulWidget {
   @override
@@ -8,6 +9,16 @@ class CalendarScreen extends StatefulWidget {
 }
 
 class CalendarScreenState extends State<CalendarScreen> {
+  CalendarController _controller;
+  Map<DateTime, List<dynamic>> _events;
+  List<dynamic> _selectedEvents;
+  void initState() {
+    super.initState();
+    _controller = CalendarController();
+    _events = {};
+    _selectedEvents = [];
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -21,37 +32,91 @@ class CalendarScreenState extends State<CalendarScreen> {
           },
         ),
       ),
-      body: SafeArea(
+      body: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: Text(
-                'Calendar Timeline',
-                style: Theme.of(context)
-                    .textTheme
-                    .headline6
-                    .copyWith(color: Colors.black),
+          children: [
+            TableCalendar(
+              calendarController: _controller,
+              initialCalendarFormat: CalendarFormat.month,
+              calendarStyle: CalendarStyle(
+                  todayColor: globals.colorSelected.getColor(),
+                  selectedColor: globals.colorSelected.getColor(),
+                  todayStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22.0,
+                      color: Colors.white)),
+              startingDayOfWeek: StartingDayOfWeek.sunday,
+              builders: CalendarBuilders(
+                selectedDayBuilder: (context, date, events) => Container(
+                    margin: const EdgeInsets.all(5.0),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(8.0)),
+                    child: Text(
+                      date.day.toString(),
+                      style: TextStyle(color: Colors.white),
+                    )),
+                todayDayBuilder: (context, date, events) => Container(
+                    margin: const EdgeInsets.all(5.0),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: globals.colorSelected.getColor(),
+                        borderRadius: BorderRadius.circular(8.0)),
+                    child: Text(
+                      date.day.toString(),
+                      style: TextStyle(color: Colors.white),
+                    )),
               ),
+              onDaySelected: (date, events, holidays) {
+                setState(() {
+                  _selectedEvents = events;
+                });
+              },
             ),
-            CalendarTimeline(
-              initialDate: DateTime(2020, 2, 20),
-              firstDate: DateTime(2020, 2, 15),
-              lastDate: DateTime(2021, 11, 20),
-              onDateSelected: (date) => print(date),
-              leftMargin: 20,
-              monthColor: globals.colorSelected.getColor(),
-              dayColor: globals.colorSelected.getColor(),
-              dayNameColor: Color(0xFF333A47),
-              activeDayColor: Colors.white,
-              activeBackgroundDayColor: Colors.redAccent[100],
-              dotsColor: globals.colorSelected.getColor(),
-              selectableDayPredicate: (date) => date.day != 23,
-            ),
+            //this is what actually displays the events below the calendar when
+            //a day is selected
+            ..._selectedEvents.map((event) => Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: MediaQuery.of(context).size.height / 20,
+                    width: MediaQuery.of(context).size.width / 2,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Colors.white,
+                        border: Border.all(color: Colors.grey)),
+                    child: Center(
+                        child: Text(
+                      event,
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16),
+                    )),
+                  ),
+                )),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: refreshCalendar,
+        child: Icon(Icons.refresh),
+      ),
     );
+  }
+
+  //This function is called when the calendar is created and it goes through the
+  //list of Agendas in the agendaDisplay List and adds their contents to their
+  //respective creation dates on the calendar.
+  void refreshCalendar() {
+    for (Agenda agenda in globals.agendaDisplay) {
+      setState(() {
+        if (_events[agenda.getCreationDate()] != null) {
+          _events[agenda.getCreationDate()].add([agenda.itemString()]);
+        } else {
+          _events[agenda.getCreationDate()] = [agenda.itemString()];
+        }
+      });
+    }
   }
 }
